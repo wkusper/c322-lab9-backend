131db0f66ab6850c70225a61b2a856331a1a571a
hbahramian <habahram@iu.edu>
2024-04-04 09:59:58 -0400

    Initial commit

---
+++ src/main/java/edu/iu/habahram/ducksservice/controllers/AuthenticationController.java
@@ -0,0 +1,23 @@
+package edu.iu.habahram.ducksservice.controllers;
+
+import edu.iu.habahram.ducksservice.model.Customer;
+import edu.iu.habahram.ducksservice.repository.CustomerRepository;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@CrossOrigin
+public class AuthenticationController {
+    CustomerRepository customerRepository;
+    public AuthenticationController(CustomerRepository
+                                            customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+    @PostMapping("/signup")
+    public void signup(@RequestBody Customer customer) {
+        try {
+            customerRepository.save(customer);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
